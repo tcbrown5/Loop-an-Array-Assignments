2998b92518bb2a44d4b1ec8444fa04db27e847dd
tcbrown5 <107014856+tcbrown5@users.noreply.github.com>
2022-06-20 17:45:23 -0500

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,8 @@
+//Create a program that lists every 3rd number between 1-20 and prints it to the console.
+
+for(let i = 0; i < 20; i++)
+{
+    if ( i % 3 === 0 ){
+        console.log(i);
+    }
+}
